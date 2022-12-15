c874384e88c3cc56989c2e4c7c5f6006ee2d3211
shree <shree@DESKTOP-4GR10UU>
2022-12-15 23:29:33 +0530

    space change

---
+++ src/Demo.java
@@ -0,0 +1,16 @@
+
+public class Demo 
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		// new change
+		int b=20;
+		int c=30;
+		int d=40;
+		
+//		Approch1
+		int [] ar= {10,20,30,40,50,60,70};
+		String [] mock= {"Mahesh", "Ram", "Ganesh", "Pallavi"};
+	}
+}
